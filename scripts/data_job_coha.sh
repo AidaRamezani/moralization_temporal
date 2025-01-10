2d39e6c70bc4aeb581f23c574995bd5057ec8933
AidaRamezani <aidaramezani9776@gmail.com>
2025-01-10 13:14:46 -0800

    data scripts

---
+++ scripts/data_job_coha.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --mem=140G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=30:0:0    
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --array=185-200
+
+cd $project/moralization_temporal
+module purge
+module load python/3.10 scipy-stack
+source ~/venv2/bin/activate
+
+year=$(($SLURM_ARRAY_TASK_ID * 10))
+model='bert-base-uncased'
+python SWOW_prediction/data_preprocessing.py --data coha --year $year --model $model --function encoding --length 200 ;
+python SWOW_prediction/data_preprocessing.py --data coha --year $year --model $model --function embedding --length 200 ;
+python SWOW_prediction/data_preprocessing.py --data coha --year $year --model $model --function graph --length 200 ;
